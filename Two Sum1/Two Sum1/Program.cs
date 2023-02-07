9ccb3c01fdbdd42cb1ce8978650e22feb725f019
SANADASHRAF <76807340+SANADASHRAF@users.noreply.github.com>
2023-02-07 15:33:29 +0200

    . Two Sum

---
+++ Two Sum1/Two Sum1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp 
+{
+    
+    public class Program
+    {
+        public  int[] TwoSum(int[] nums, int target)
+        {
+            var dictionary = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int x = nums[i];
+                if (dictionary.ContainsKey(target - x))
+                {
+                    return new int[] { dictionary[target - x], i };
+                }
+                dictionary[x] = i;
+            }
+            return null;
+        }
+
+       
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int t = 9;
+            
+          Console.WriteLine(  );
+        }
+    }
+}
