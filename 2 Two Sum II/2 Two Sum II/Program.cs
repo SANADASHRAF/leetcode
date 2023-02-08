77cd093acbade385e7cea333d842e8cd4057e7e8
SANADASHRAF <76807340+SANADASHRAF@users.noreply.github.com>
2023-02-08 15:08:16 +0200

    Two Sum II

---
+++ 2 Two Sum II/2 Two Sum II/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyApp 
+{
+
+    public class Program
+    {
+        
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int j = BinarySearch(numbers, target - numbers[i]);
+                if (j != -1)
+                {
+                    return new int[] { i + 1, j + 1 };
+                }
+            }
+            return null;
+        }
+        public int BinarySearch(int[] nums, int key)
+        {
+            int first = 0;
+            int last = nums.Length - 1;
+            int mid = 0;
+            while (first <= last)
+            {
+                mid = (first + last) / 2;
+                if (nums[mid] == key) 
+                    return mid;
+                else if (nums[mid] < key)
+                    first = mid + 1;
+                else 
+                    last = mid - 1;
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
