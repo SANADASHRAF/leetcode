fa1aa84b6d155c77b81ac5ecdb4d01946bfd42f9
SANADASHRAF <76807340+SANADASHRAF@users.noreply.github.com>
2023-02-10 19:41:39 +0200

    Palindrome Number

--- 2 Two Sum II/2 Two Sum II/Program.cs
+++ 2 Two Sum II/2 Two Sum II/Program.cs
@@ -5,13 +5,15 @@ namespace MyApp
 
     public class Program
     {
-        
+
         public int[] TwoSum(int[] numbers, int target)
         {
-            
+
             for (int i = 0; i < numbers.Length; i++)
             {
                 int j = BinarySearch(numbers, target - numbers[i]);
+                if (j == i)
+                    return new int[] { i + 1, j + 2 };
                 if (j != -1)
                 {
                     return new int[] { i + 1, j + 1 };
@@ -21,17 +23,17 @@ namespace MyApp
         }
         public int BinarySearch(int[] nums, int key)
         {
-            int first = 0;
+            int first = 1;
             int last = nums.Length - 1;
             int mid = 0;
             while (first <= last)
             {
                 mid = (first + last) / 2;
-                if (nums[mid] == key) 
+                if (nums[mid] == key)
                     return mid;
                 else if (nums[mid] < key)
                     first = mid + 1;
-                else 
+                else
                     last = mid - 1;
             }
             return -1;
---
+++ Palindrome Number/Palindrome Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+
+        public static bool IsPalindrome(int x)
+        {
+            var y=x.ToString();
+            y.Reverse();
+
+            return x.ToString()==y;
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Program.IsPalindrome(121));
+        }
+    }
+}
