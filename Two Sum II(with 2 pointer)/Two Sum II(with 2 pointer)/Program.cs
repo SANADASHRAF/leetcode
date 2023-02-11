7d6ad82191c87e9082d654034f7399d679a636d7
SANADASHRAF <76807340+SANADASHRAF@users.noreply.github.com>
2023-02-11 17:17:19 +0200

    Valid Palindrome

--- Two Sum II(with 2 pointer)/Two Sum II(with 2 pointer)/Program.cs
+++ Two Sum II(with 2 pointer)/Two Sum II(with 2 pointer)/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp 
+{
+    public class Program
+    {
+
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int l = 0, r = numbers.Length - 1;
+            for (int i = 1; i <= numbers.Length; i++)
+            {
+                if (numbers[l] + numbers[r] == target) return new int[] { l + 1, r + 1 };
+                else if (numbers[l] + numbers[r] < target) l++;
+                else r--;
+            }
+            return null;
+        }
+        static void Main(string[] args)
+        {
+
+           
+           
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Valid Palindrome/Valid Palindrome/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static bool IsPalindrome(string s)
+        {
+            string z = "", x = "";
+            int l = 0, r = s.Length - 1;
+            s = s.ToLower();
+            if (s == " ")
+                return true;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsLetterOrDigit(s[i]))
+                    z += s[i];
+
+            }
+            for (int i = z.Length - 1; i >= 0; i--)
+            {
+
+                x += z[i];
+
+            }
+            if (x.Equals(z))
+                return true;
+            else
+                return false;
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
