9d1f213c714316016b9de6872e5913cb414b2c51
SANADASHRAF <76807340+SANADASHRAF@users.noreply.github.com>
2023-02-13 00:28:51 +0200

    Reverse Words in a String

---
+++ Reverse Words in a String/Reverse Words in a String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static String ReverseWords(String s)
+        {
+           
+            {
+                return String.Join(" ", s.Split(' ', StringSplitOptions.RemoveEmptyEntries).Reverse());
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.ReverseWords("the   sky is blue"));
+           
+        }
+    }
+}
